2d68829b6128284f5df7adaa889a6b7df4b18355
gabriel_andrad <gabriel.nunes@tegra.com.br>
2023-01-04 09:46:10 -0300

    ajustes

--- src/pages/home/home/maps/index.tsx
+++ src/pages/home/home/maps/index.tsx
@@ -5,11 +5,9 @@ const Maps = () => {
   
   return (
     <div className="map-container">
-        <iframe width="100%" height="300" 
-        frameBorder="0" scrolling="no" marginHeight={0} marginWidth={0} id="gmap_canvas" 
+            <iframe title="maps" width="100%" height="300" frameBorder="0"
         src="https://maps.google.com/maps?width=577&amp;height=300&amp;hl=en&amp;q=Rua%20Dionisio%20Reis%20dos%20santos%20n200%20Sorocaba+(Caverna%20Barbearia)
         &amp;t=&amp;z=18&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
-
 
     </div>
   )
