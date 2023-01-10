b4e58477b42b49b1503743709a1c9fcf7751e2cd
gabriel_andrad <gabriel.nunes@tegra.com.br>
2023-01-10 16:20:46 -0300

    ajustes

--- src/pages/home/home/schedule-end/index.tsx
+++ src/pages/home/home/schedule-end/index.tsx
@@ -19,7 +19,7 @@ const ModalScheduleEnd:React.FunctionComponent<IProps> = ({
       </div>
        <div className="modal-confirm-end">
         <span>Código do agendamento</span>
-        <h1>{scheduleId}</h1>
+        <h1 style={{color:'#000'}}>{scheduleId}</h1>
         <span className="line-text-confirm">
           O código do agendamento é utilizado para cancelar o agendamento ou para realizar uma consulta das informações do agendamento na opção Minha Conta. 
           <br/>
