9c5618dfb2d90d0c5b6f78231f736c34930d5f72
Gabriel <gabriel.nunes@meucliente.app.br>
2025-01-06 09:51:06 -0300

    ajuste

--- src/pages/home/home/schedule/calendar/indext.tsx
+++ src/pages/home/home/schedule/calendar/indext.tsx
@@ -31,8 +31,8 @@ const CalendarDate:React.FunctionComponent<Iprops> =({
         maxDate={new Date(dateD.setDate(dateF.getDate() + 30))}
         locale={'pt-BR'}
         tileDisabled={(date) => (
-          date.date.getDate() === 23 || date.date.getDate() === 22 || date.date.getDate() === 24 || date.date.getDate() === 29?false:
-          (date.date.getDay() === 0) || date.date.getDay() === 1 || date.date.getDate() === 1 || date.date.getDate() === 31
+          // date.date.getDate() === 23 || date.date.getDate() === 22 || date.date.getDate() === 24 || date.date.getDate() === 29?false:
+          (date.date.getDay() === 0) || date.date.getDay() === 1
           // (date.date.getMonth() === 11 && (date.date.getDate() >= 15 && date.date.getDate() <= 31))
         )
             
