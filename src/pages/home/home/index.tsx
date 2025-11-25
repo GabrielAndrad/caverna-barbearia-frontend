d38ca44af60a105485e3cb8867e831b9cb5cd98c
Caio Andrade <caio.nune@meucliente.app.br>
2025-11-24 23:08:39 -0300

    ajuste site

--- src/pages/home/home/index.tsx
+++ src/pages/home/home/index.tsx
@@ -1,18 +1,43 @@
 import React from 'react'
 import background from '../../../assets/background-barbearia.jpeg'
 import logo from '../../../assets/logo.png'
+// import logoNatal from '../../../assets/NatalLogo.png'
 import '../index.scss'
 import Maps from './maps'
 import Footer from '../../../components/Footer'
 import Schedule from './schedule'
 import store from '../../../store/home-store'
 import CarrouselComponent from './carrousel'
-import CountdownTimer from '../../Cronometro'
+// import CountdownTimer from '../../Cronometro'
 // import topnatal from '../../../assets/bordanatal.png'
 // import sinoa from '../../../assets/sinoa.png'
 interface IProps {
   changeMenu: (menu) => void
 }
+
+// const Snowflakes: React.FC = () => {
+//   const snowflakes = Array.from({ length: 50 }, (_, i) => i);
+  
+//   return (
+//     <div className="snowflakes-container">
+//       {snowflakes.map((flake) => (
+//         <div
+//           key={flake}
+//           className="snowflake"
+//           style={{
+//             left: `${Math.random() * 100}%`,
+//             animationDuration: `${Math.random() * 8 + 8}s`,
+//             animationDelay: `${Math.random() * 8}s`,
+//             opacity: Math.random() * 0.6 + 0.4,
+//             fontSize: `${Math.random() * 10 + 10}px`
+//           }}
+//         >
+//           ❄
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
 
 const Home:React.FunctionComponent<IProps> = ({
   changeMenu
@@ -20,12 +45,14 @@ const Home:React.FunctionComponent<IProps> = ({
   const {showSchedule,setShowSchedule} = store()
   return (
     <div>
+      {/* <Snowflakes /> */}
       <section className="background"> 
       {/* <img className='topo' src={topnatal} alt=""/>
-      <img className='sinoleft' src={sinoa} alt=""/>
-      <img className='sinoright'src={sinoa} alt=""/> */}
+      // <img className='sinoleft' src={sinoa} alt=""/>
+      // <img className='sinoright'src={sinoa} alt=""/> */}
         <img className="background" src={background} alt=""/>
         <img className = "logo" src={logo} alt=""/>
+        {/* <img className = "logo-natal" src={logoNatal} alt=""/> */}
       </section>
       <section className="schedule-time">
         <span className="title">CAVERNA BARBEARIA</span>
@@ -33,7 +60,7 @@ const Home:React.FunctionComponent<IProps> = ({
         <button onClick={() => setShowSchedule(true)}>AGENDAR HORÁRIO</button>
       </section>
       <section>
-        <CountdownTimer/>
+        {/* <CountdownTimer/> */}
       {!showSchedule && <CarrouselComponent/>}
       {!showSchedule && <Maps/>}
 
--- src/pages/home/home/schedule/calendar/indext.tsx
+++ src/pages/home/home/schedule/calendar/indext.tsx
@@ -30,14 +30,21 @@ const CalendarDate:React.FunctionComponent<Iprops> =({
         minDate={new Date()} 
         maxDate={new Date(dateD.setDate(dateF.getDate() + 30))}
         locale={'pt-BR'}
-        tileDisabled={(date) => (
-          // date.date.getDate() === 23 || date.date.getDate() === 22 || date.date.getDate() === 24 || date.date.getDate() === 29?false:
-          (date.date.getDay() === 0) || date.date.getDay() === 1
-          // (date.date.getMonth() === 11 && (date.date.getDate() >= 15 && date.date.getDate() <= 31))
-        )
-            
-      
-        }
+        tileDisabled={({ date }) => {
+          const isSundayOrMonday = date.getDay() === 0 || date.getDay() === 1;
+
+          const isDec2025Closed =
+            date.getFullYear() === 2025 &&
+            date.getMonth() === 11 &&
+            [25, 31].includes(date.getDate());
+
+          const isJan2026Closed =
+            date.getFullYear() === 2026 &&
+            date.getMonth() === 0 &&
+            [1, 2, 3, 4, 5, 6].includes(date.getDate());
+
+          return isSundayOrMonday || isDec2025Closed || isJan2026Closed;
+        }}
       />
     </div>
     
